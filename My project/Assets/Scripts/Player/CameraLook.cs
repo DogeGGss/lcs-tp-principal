20d03a9339ab2233ef150c4c945b92e617e8ba17
NahuVelazquez <nahuel605.nv@gmail.com>
2026-09-22 19:48:13 -0300

    US01, US03, US05, US07, US08

---
+++ My project/Assets/Scripts/Player/CameraLook.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class CameraLook : MonoBehaviour
+{
+
+    public float mouseSensivility = 80f;
+
+    public Transform playerBody;
+
+    float xRotation = 0;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X")*mouseSensivility * Time.deltaTime;
+
+        float mouseY = Input.GetAxis("Mouse Y")*mouseSensivility * Time.deltaTime;
+
+        xRotation -= mouseY;
+
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+
+        playerBody.Rotate(Vector3.up * mouseX);
+
+    }
+}
---
+++ My project/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+     private CharacterController controller;
+
+    public float speed = 5f;
+    public float gravity = -9.8f;
+    public float jumpForce = 5f;
+
+    private float verticalVelocity;
+
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x
+                     + transform.forward * z;
+
+        if (controller.isGrounded && verticalVelocity < 0)
+        {
+            verticalVelocity = -2f;
+        }
+
+        verticalVelocity += gravity * Time.deltaTime;
+
+        Vector3 velocity = move * speed;
+        velocity.y = verticalVelocity;
+
+        controller.Move(velocity * Time.deltaTime);
+
+        if (Input.GetButtonDown("Jump") && controller.isGrounded)
+        {
+            verticalVelocity = jumpForce;
+        }
+    }
+}
